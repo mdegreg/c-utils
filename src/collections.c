c028e53717fc0b5a29da58bd4bb4eef3967af397
Matthew DeGregorio <mdegregorio01@gmail.com>
2024-03-25 21:51:28 -0500

    Add tests and refinements to underlying code file. Figured out pointer and struct initialization struggles, as well as missing logical check for first added element.

--- src/collections.c
+++ src/collections.c
@@ -7,11 +7,8 @@
 
 linked_list_t create_linked_list(void){
     linked_list_t new_list;
-    node_t * node;
-    node = (node_t *) malloc(sizeof(node_t));
-    node->data = NULL;
-    new_list.head = node;
-    new_list.tail = node;
+    new_list.head = NULL;
+    new_list.tail = NULL;
     return new_list;
 }
 
@@ -19,8 +16,15 @@ void add_linked_list_head(linked_list_t * list, void * value){
     node_t * new_head;
     new_head = (node_t *) malloc(sizeof(node_t));
     new_head->data = value;
-    new_head->next = list->head;
+    if (list->head != NULL) {
+        new_head->next = list->head;
+    }
     list->head = new_head;
+    if (list->tail == NULL) {
+        // If tail doesn't exist, then a new head will also
+        // be the new tail.
+        list->tail = new_head;
+    }
 }
 
 void add_linked_list_tail(linked_list_t * list, void * value){
@@ -28,7 +32,14 @@ void add_linked_list_tail(linked_list_t * list, void * value){
     new_tail = (node_t *) malloc(sizeof(node_t));
     new_tail->data = value;
     if (list->tail != NULL) {
+        // We attach the new node to the existing tail, if it exists--or skip to avoid
+        // a bus error.
         list->tail->next = new_tail;
+    }
+    if (list->head == NULL) {
+        // If head doesn't exist, then a new tail will also
+        // be the new head.
+        list->head = new_tail;
     }
     list->tail = new_tail;
 }
--- tests/test_collections.cpp
+++ tests/test_collections.cpp
@@ -9,8 +9,8 @@
 TEST(LinkedList, LinkedListCreate) {
     linked_list_t test_list;
     test_list = create_linked_list();
-    EXPECT_TRUE(test_list.head->data == nullptr);
-    EXPECT_TRUE(test_list.tail->data == nullptr);
+    EXPECT_TRUE(test_list.head == nullptr);
+    EXPECT_TRUE(test_list.tail == nullptr);
 }
 
 TEST(LinkedList, LinkedListAddHead) {
@@ -20,6 +20,13 @@ TEST(LinkedList, LinkedListAddHead) {
     new_val = 3;
     add_linked_list_head(&test_list, &new_val);
     EXPECT_EQ(* (int*) test_list.head->data, new_val);
+    EXPECT_EQ(* (int*) test_list.tail->data, new_val);
+    int second_val;
+    second_val = 5;
+    add_linked_list_head(&test_list, &second_val);
+    EXPECT_EQ(* (int*) test_list.head->data, second_val);
+    EXPECT_EQ(* (int*) test_list.head->next->data, new_val);
+
 }
 TEST(LinkedList, LinkedListAddTail) {
     linked_list_t test_list;
@@ -28,4 +35,10 @@ TEST(LinkedList, LinkedListAddTail) {
     new_val = 3;
     add_linked_list_tail(&test_list, &new_val);
     EXPECT_EQ(* (int*) test_list.tail->data, new_val);
+    EXPECT_EQ(* (int*) test_list.head->data, new_val);
+    int second_val;
+    second_val = 25;
+    add_linked_list_tail(&test_list, &second_val);
+    EXPECT_EQ(* (int*) test_list.tail->data, second_val);
+    EXPECT_EQ(* (int*) test_list.head->data, new_val);
 }
